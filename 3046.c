8479b874411e0ddd646ab81a7f7c3522318b22f4
eduardojulio <ejulio148@gmail.com>
2021-05-27 11:23:07 -0300

    exercicio 3046

---
+++ 3046.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n, resultado;
+    scanf("%d", &n);
+
+    resultado = ((n+1)*(n+2))/2;
+
+    printf("%d", resultado);
+    system("pause");
+    return 0;
+}
