3d823e8a27255fedd1e50e50c90077e1d0840653
eduardojulio <ejulio148@gmail.com>
2021-06-20 20:45:33 -0300

    versão 2 do exercicio 1021

--- 1021.c
+++ 1021.c
@@ -1,116 +1,30 @@
 #include <stdio.h>
 
-int main()
-{
-    int valor_notas = 0, valor_moedas = 0, temp = 0;
-    int nota100 = 0, nota50 = 0, nota20 = 0, nota10 = 0, nota5 = 0, nota2 = 0;
-    int moeda100 = 0, moeda50 = 0, moeda25 = 0, moeda20 = 0, moeda10 = 0, moeda5 = 0, moeda1 = 0;
+int main(){
 
-    // Entrada de dados
-    scanf("%d.%d", &valor_notas, &valor_moedas);
+    double notas[] = {100.00, 50.00, 20.00, 10.00, 5.00, 2.00}, moedas[] = {1.00, 0.50, 0.25, 0.10, 0.05, 0.01};
+    int notasint[] = {100, 50, 20, 10, 5, 2}, moedasint[] = {100, 50, 25, 10, 5, 1};
+    int i, qtdNotas[6], qtdMoedas[6];
+    int vNota, vMoeda;
 
-    // Calculo
-    if (valor_notas >= 100)
-    {
-        temp = valor_notas % 100;
-        nota100 = (valor_notas - temp) / 100;
-        valor_notas = temp;
-    }
-
-    if (valor_notas >= 50)
-    {
-        temp = valor_notas % 50;
-        nota50 = (valor_notas - temp) / 50;
-        valor_notas = temp;
-    }
-
-    if (valor_notas >= 20)
-    {
-        temp = valor_notas % 20;
-        nota20 = (valor_notas - temp) / 20;
-        valor_notas = temp;
-    }
-
-    if (valor_notas >= 10)
-    {
-        temp = valor_notas % 10;
-        nota10 = (valor_notas - temp) / 10;
-        valor_notas = temp;
-    }
-
-    if (valor_notas >= 5)
-    {
-        temp = valor_notas % 5;
-        nota5 = (valor_notas - temp) / 5;
-        valor_notas = temp;
-    }
-
-    if (valor_notas >= 2)
-    {
-        temp = valor_notas % 2;
-        nota2 = (valor_notas - temp) / 2;
-        valor_notas = temp;
-    }
-
-    if (valor_notas == 1){
-        valor_moedas += 100;
-        valor_notas -= 1;
-    }
-
-    if (valor_moedas >= 100)
-    {
-        temp = valor_moedas % 100;
-        moeda100 = (valor_moedas - temp) / 100;
-        valor_moedas = temp;
-    }
-
-    if (valor_moedas >= 50)
-    {
-        temp = valor_moedas % 50;
-        moeda50 = (valor_moedas - temp) / 50;
-        valor_moedas = temp;
-    }
-
-    if (valor_moedas >= 25)
-    {
-        temp = valor_moedas % 25;
-        moeda25 = (valor_moedas - temp) / 25;
-        valor_moedas = temp;
-    }
-
-    if (valor_moedas >= 10)
-    {
-        temp = valor_moedas % 10;
-        moeda10 = (valor_moedas - temp) / 10;
-        valor_moedas = temp;
-    }
-
-    if (valor_moedas >= 5)
-    {
-        temp = valor_moedas % 5;
-        moeda5 = (valor_moedas - temp) / 5;
-        valor_moedas = temp;
-    }
-
-    if (valor_moedas >= 1)
-    {
-        moeda1 = valor_moedas;
-    }
+    scanf("%d.%d", &vNota, &vMoeda);
 
     printf("NOTAS:\n");
-    printf("%d nota(s) de R$ 100.00\n", nota100);
-    printf("%d nota(s) de R$ 50.00\n", nota50);
-    printf("%d nota(s) de R$ 20.00\n", nota20);
-    printf("%d nota(s) de R$ 10.00\n", nota10);
-    printf("%d nota(s) de R$ 5.00\n", nota5);
-    printf("%d nota(s) de R$ 2.00\n", nota2);
-    printf("MOEDAS:\n");
-    printf("%d moeda(s) de R$ 1.00\n", moeda100);
-    printf("%d moeda(s) de R$ 0.50\n", moeda50);
-    printf("%d moeda(s) de R$ 0.25\n", moeda25); 
-    printf("%d moeda(s) de R$ 0.10\n", moeda10);
-    printf("%d moeda(s) de R$ 0.05\n", moeda5);
-    printf("%d moeda(s) de R$ 0.01\n", moeda1);
+    for(i = 0; i < 6; i++){
+        qtdNotas[i] = vNota / notasint[i];
+        vNota -= qtdNotas[i] * notasint[i];
+        printf("%d nota(s) de R$ %.2lf\n", qtdNotas[i], notas[i]);
+    }
 
+    if(vNota == 1){
+        vMoeda += 100;
+    }
+
+    printf("MOEDAS:\n");
+    for(i = 0; i < 6; i++){
+        qtdMoedas[i] = vMoeda / moedasint[i];
+        vMoeda -= qtdMoedas[i] * moedasint[i];
+        printf("%d moeda(s) de R$ %.2lf\n", qtdMoedas[i], moedas[i]);
+    }
     return 0;
-} 
+}
